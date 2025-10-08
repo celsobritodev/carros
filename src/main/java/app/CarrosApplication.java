ac23385a086ce3a3fb07b4a7fe8a79951bc0475f
celsobritodev <celsinhobrito@gmail.com>
2025-10-08 19:35:45 -0300

    crud para carros completo

--- src/main/java/app/CarrosApplication.java
+++ src/main/java/app/CarrosApplication.java
@@ -3,6 +3,10 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Curso - Spring Boot e Angular | 19 - Angular 17: Relacionamento N para 1 (front e back)
+// https://youtu.be/zJFVhlwdwvg?list=PLtII2Mw41oA0LLJgLVeyRWO5IPUtdfRth
+
+
 @SpringBootApplication
 public class CarrosApplication {
 
